6be32122d62bbbfe6f443f3807efdbbd8c31c406
Piyush Verma <pverma@jackhenry.com>
2025-09-02 09:19:42 -0400

    Rename class names

---
+++ CardTokenization/EncryptionService.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+internal interface IEncryptionService
+{
+    TokenEntity EncryptAsync(string cardNumber);
+    TokenEntity DecryptAsync(string token);
+}
+
+internal class EncryptionService : IEncryptionService
+{
+    public TokenEntity EncryptAsync(string cardNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TokenEntity DecryptAsync(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
