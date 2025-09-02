e6a5ccf3d69701c14b03d1ab62f41dd78a20c3c8
Piyush Verma <pverma@jackhenry.com>
2025-09-02 14:55:22 -0400

    Bin

--- CardTokenization/TokenEntity.cs
+++ CardTokenization/TokenEntity.cs
@@ -2,7 +2,7 @@
 internal class TokenEntity
 {
     public long CardID { get; set; }
-    private string Bin { get; set; }
+    public string Bin { get; set; }
     public string FpeCardNumber { get; set; }
     public string Token { get; set; }
     public DateTime CreatedUtc { get; set; }
